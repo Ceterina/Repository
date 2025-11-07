48a28af3fb0c94e48c3396438cf6c89ac9c3f270
Ceterina <helenko.liliia@gmail.com>
2025-11-07 23:45:15 +0200

    HomeWork6_Helenko
    Constraints

---
+++ HomeWork6_Helenko.sql
@@ -0,0 +1,56 @@
+﻿--IF OBJECT_ID ('WAREHOUSE') IS NOT NULL
+--	DROP SCHEMA WAREHOUSE
+--GO
+
+
+--CONSTRAINT PK_Products PRIMARY KEY CLUSTERED (ProductID),
+--	CONSTRAINT UQ_Products_SKU UNIQUE (SKU),
+--	CONSTRAINT CK_products_Price CHECK (Price >= 0),
+--	CONSTRAINT DF_Products_Status DEFAULT('InStock') FOR [Status]
+--	);
+
+
+
+
+IF OBJECT_ID ('Warehouse.Products', 'U') IS NOT NULL
+	DROP TABLE Warehouse.Products
+GO
+
+IF EXISTS (SELECT * FROM SYS.SCHEMAS WHERE name = 'Warehouse')
+	DROP SCHEMA WAREHOUSE
+GO
+
+CREATE SCHEMA Warehouse
+GO
+
+
+CREATE TABLE Warehouse.Products (
+	ProductID INT IDENTITY (1,1)  NOT NULL,
+	SKU		  NVARCHAR(20)	      NOT NULL  UNIQUE,
+	Price	  DECIMAL(10, 2)      NOT NULL  CHECK (Price >= 0),
+	[Status]  NVARCHAR(50)        NOT NULL
+	);
+ALTER TABLE Warehouse.Products
+  ADD CONSTRAINT PK_Products PRIMARY KEY CLUSTERED (ProductID);
+
+ALTER TABLE Warehouse.Products
+  ADD CONSTRAINT UQ_Products_SKU UNIQUE (SKU);
+
+ALTER TABLE Warehouse.Products
+  ADD CONSTRAINT CK_Products_Price CHECK (Price > 0);
+
+ALTER TABLE Warehouse.Products
+  ADD CONSTRAINT DF_Products_Status DEFAULT (N'InStock') FOR [Status];
+GO
+
+
+INSERT INTO Warehouse.Products (SKU, Price)
+VALUES ('MB-M1',  1000),
+	   ('MB-M2',  1300),
+	   ('MB-M3',  1500),
+	   ('MB-M4',  1700),
+	   ('MB-Pro', 2000);
+
+SELECT *
+	FROM [Warehouse].[Products];
+GO
